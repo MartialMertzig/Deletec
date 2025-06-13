e8f36de3c06c075bc45221fa92183c8a2dda302e
MartialMertzig <138697506+MartialMertzig@users.noreply.github.com>
2025-06-13 15:46:09 +0200

    Création de vues API pour obtenir les données stockés en data base en format JSON

--- inventory_system/inventory/views.py
+++ inventory_system/inventory/views.py
@@ -2,3 +2,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Bienvenue dans le système de gestion d'inventaire !")
+
+from rest_framework import viewsets
+from.models import Product, Productrequest
+from .serializers import ProductSerializer, ProductRequestSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductRequestViewSet(viewsets.ModelViewSet):
+    queryset = Productrequest.objects.all()
+    serializer_class = ProductRequestSerializer
