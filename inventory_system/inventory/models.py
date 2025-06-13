14bf0221f5ae21bab2cde6317aa5d75d501aa65c
MartialMertzig <138697506+MartialMertzig@users.noreply.github.com>
2025-06-13 15:01:40 +0200

    Ajout des status dans les produits

--- inventory_system/inventory/models.py
+++ inventory_system/inventory/models.py
@@ -10,3 +10,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name 
+    
+from django.db import models
+from django.contrib.auth.models import User
+from .models import Product
+
+class Productrequest(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'En attente'),
+        ('approved', 'Approuvée'),
+        ('rejected', 'Refusée'),
+    ]
