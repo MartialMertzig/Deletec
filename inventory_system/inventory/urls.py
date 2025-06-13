c11c5acc7d4fd9c9cdddd427a7271dc5f591ad11
MartialMertzig <138697506+MartialMertzig@users.noreply.github.com>
2025-06-13 15:55:42 +0200

    Création des routes pour les vues API Produits et Demande dans inventory/urls.py

--- inventory_system/inventory/urls.py
+++ inventory_system/inventory/urls.py
@@ -4,3 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from views import ProductViewSet, ProductRequestViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'requests', ProductRequestViewSet)
+
+urlpartterns = [
+    path('api/', include(router.urls)),
+]
